bbc3adf976c95cc9e916086c89030fef8336b8fa
wx759 <inxwen@outlook.com>
2025-12-29 11:57:14 +0800

    Revert "将最终版本TD3加入库【为了让ai改代码"
    This reverts commit 7c7344d914586a7ebee6b0864892ed76e3d165d6.

--- Agent/TD3.py
+++ Agent/TD3.py
@@ -7,27 +7,25 @@ import math
 import copy
 import os
 from Agent.Common.ExperienceReplay_TD3 import Experience_Replay as ExpRep
-from . import Config
+from . import  Config
 from Agent.RuningMeanStd import RunningMeanStd
-
 # from Agent.RuningMeanStd import TfRunningMeanStd
 
 tranLock = True
 isPercent = True
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
 class Actor(nn.Module):
-    def __init__(self, s_dim, a_dim, a_bound):
+    def __init__(self,s_dim,a_dim,a_bound):
         super(Actor, self).__init__()
 
-        self.l1 = nn.Linear(s_dim, 128)
-        self.l2 = nn.Linear(128, 32)
-        self.l3 = nn.Linear(32, a_dim)
+        self.l1 = nn.Linear(s_dim,128)
+        self.l2 = nn.Linear(128,32)
+        self.l3 = nn.Linear(32,a_dim)
 
         self.a_bound = a_bound
 
-    def forward(self, state):
+    def forward(self,state):
         state = torch.FloatTensor(state).to(device)
         a = F.tanh(self.l1(state))
         a = F.leaky_relu(self.l2(a))
@@ -35,24 +33,24 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
-    def __init__(self, s_dim, a_dim):
-        super(Critic, self).__init__()
+    def __init__(self,s_dim,a_dim):
+        super(Critic,self).__init__()
 
-        # Q1
-        self.l1 = nn.Linear(s_dim + a_dim, 128)
-        self.l2 = nn.Linear(128, 32)
-        self.l3 = nn.Linear(32, 1)
+        #Q1
+        self.l1 = nn.Linear(s_dim+a_dim,128)
+        self.l2 = nn.Linear(128,32)
+        self.l3 = nn.Linear(32,1)
 
-        # Q2
-        self.l4 = nn.Linear(s_dim + a_dim, 128)
-        self.l5 = nn.Linear(128, 32)
-        self.l6 = nn.Linear(32, 1)
+        #Q2
+        self.l4 = nn.Linear(s_dim+a_dim,128)
+        self.l5 = nn.Linear(128,32)
+        self.l6 = nn.Linear(32,1)
 
-    def forward(self, state, action):
+    def forward(self,state,action):
         state = torch.FloatTensor(state).to(device)
         action = torch.FloatTensor(action).to(device)
 
-        sa = torch.cat([state, action], 1)
+        sa = torch.cat([state,action],1)
 
         q1 = F.leaky_relu(self.l1(sa))
         q1 = F.leaky_relu(self.l2(q1))
@@ -62,22 +60,22 @@ class Critic(nn.Module):
         q2 = F.leaky_relu(self.l5(q2))
         q2 = self.l6(q2)
 
-        return q1, q2
+        return q1,q2
 
-    def Q1(self, state, action):
+
+    def Q1(self,state,action):
         state = torch.FloatTensor(state).to(device)
         action = torch.FloatTensor(action).to(device)
 
-        sa = torch.cat([state, action], 1)
+        sa = torch.cat([state,action],1)
 
         q1 = F.leaky_relu(self.l1(sa))
         q1 = F.leaky_relu(self.l2(q1))
         q1 = self.l3(q1)
         return q1
 
-
 class TD3(object):
-    def __init__(self, config: Config):
+    def __init__(self,config:Config):
         self.a_dim = config.action_dim
         self.s_dim = config.state_dim
         self.a_bound = config.action_bound
@@ -92,7 +90,7 @@ class TD3(object):
         self.GAMMA = config.REWARD_GAMMA
         self.TAU = config.SOFT_REPLACE_TAU
         self.BATCH_SIZE = config.BATCH_SIZE
-        # self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+        #self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
         self.pointer = 0
         # self.noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(self.a_dim))
         self.episode_temp = {}
@@ -101,18 +99,18 @@ class TD3(object):
         self.is_delay = config.IS_ACTOR_UPDATE_DELAY
         self.is_double = config.IS_CRITIC_DOUBLE_NETWORK
         self.is_smooth = config.IS_QNET_SMOOTH_CRITIC
-        self.update_cnt = 0  # 更新次数
-        self.rms = RunningMeanStd(epsilon=0.0, shape=self.s_dim)
-        self.is_rms = False
+        self.update_cnt = 0 # 更新次数
+        self.rms = RunningMeanStd(epsilon=0.0,shape=self.s_dim)
+        self.is_rms=False
         self.tfRms = {}
         self.toshow = {}
-        self.policy_noise = config.POLICY_NOISE
-        # LSTM参数
-        self.critic_loss = 0
-        self.actor_loss = 0
+        self.policy_noise =config.POLICY_NOISE
+        #LSTM参数
+        self.critic_loss=0
+        self.actor_loss=0
         self.discount = config.DISCOUNT
         if self.is_delay:
-            self.policy_target_update_interval = config.ACTOR_UPDATE_DELAY_TIMES  # 策略网络更新频率
+            self.policy_target_update_interval = config.ACTOR_UPDATE_DELAY_TIMES # 策略网络更新频率
         else:
             self.policy_target_update_interval = 1
 
@@ -135,14 +133,15 @@ class TD3(object):
         self.lr_a = self.LR_A
         self.lr_c = self.LR_C
 
-        # init Actor and Critic network(eval,target)
-        self.actor = Actor(self.s_dim, self.a_dim, self.a_bound).to(device)
-        self.actor_target = copy.deepcopy(self.actor)
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.lr_a)
 
-        self.critic = Critic(self.s_dim, self.a_dim).to(device)
+        #init Actor and Critic network(eval,target)
+        self.actor = Actor(self.s_dim,self.a_dim,self.a_bound).to(device)
+        self.actor_target = copy.deepcopy(self.actor)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),lr = self.lr_a)
+
+        self.critic = Critic(self.s_dim,self.a_dim).to(device)
         self.critic_target = copy.deepcopy(self.critic)
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.lr_c)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),lr = self.lr_c)
         # hard_update
         for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
             target_param.data.copy_(param.data)
@@ -150,21 +149,22 @@ class TD3(object):
         for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
             target_param.data.copy_(param.data)
 
-    def choose_action(self, h_epi, state):
-        c = np.array(state)[np.newaxis, :]
+
+
+    def choose_action(self,h_epi,state):
+        c = np.array(state)[np.newaxis,:]
         # rms 疑似不管用
         self.rms.update(c)
         if self.is_rms:
-            state = (state - self.rms.mean) / (self.rms.var + 1e-5)
+            state = (state - self.rms.mean)/(self.rms.var + 1e-5)
 
         self.var = self.var_init
-        if self.pointer > self.var_stable_at:
+        if self.pointer >self.var_stable_at:
             self.var = self.var_stable
         else:
             delta_step = self.pointer - self.var_drop_at
-            if delta_step > 0:
-                self.var = self.var_init + delta_step * (self.var_stable - self.var_init) / (
-                            self.var_stable_at - self.var_drop_at)
+            if delta_step > 0 :
+                self.var = self.var_init +delta_step * (self.var_stable - self.var_init)/(self.var_stable_at - self.var_drop_at)
         if h_epi is None:
             h_epi = self.memory.new_ep()
             self.episode_temp[h_epi] = dict()
@@ -186,7 +186,7 @@ class TD3(object):
             action = np.clip(action + np.random.normal([0 for i in range(self.a_dim)],self.var), 0.001, 100000000) # 固定值
 
         '''
-        list = [self.actor(state.reshape(1, -1))]
+        list = [self.actor(state.reshape(1,-1))]
         action = list[0][0]
         if isPercent:
             action = action.cpu().detach() + np.random.normal([0 for i in range(self.a_dim)], self.var)
@@ -199,14 +199,17 @@ class TD3(object):
             action = np.clip(action.cpu().detach() + np.random.normal([0 for i in range(self.a_dim)], self.var), 0.001,
                              100000000)  # 固定值
 
-        return h_epi, action.numpy()
+        return h_epi,action.numpy()
 
-    def episode_feedback(self, h_epi, state, action, reward, final_state):
+    def episode_feedback(self,h_epi, state, action, reward, final_state):
         if final_state is not None:
             final_state = np.zeros(len(final_state))
         self.pointer += 1
-        if self.pointer % 100 == 0:
+        if self.pointer%100 == 0:
             print(self.mark())
+
+
+
         ret_h_epi = self.memory.store_transition(h_epi, state, action, reward, final_state)
         return ret_h_epi
 
@@ -219,7 +222,7 @@ class TD3(object):
         if self.pointer < 3000:
             return 0
         self.update_cnt += 1
-        if self.pointer > 8000 and self.pointer % 100 == 0:
+        if self.pointer > 8000 and self.pointer%100 == 0:
             a = 1
 
         # 从replay buffer（通过memory调用）中随机采样的样本数据
@@ -240,12 +243,11 @@ class TD3(object):
         b_r = b_M[2].reshape(-1, 1)
         b_s_ = b_s__rm.reshape(-1, self.s_dim)
 
+
         # 根据当前训练步数（pointer）计算Critic和Actor网络的学习率lr_c，lr_a
         # 其实没什么用
-        self.lr_a = max(self.LR_A_STABLE,
-                        self.LR_A * np.power(self.LR_DECAY, ((self.pointer - 3000) / self.LR_DECAY_TIME)))
-        self.lr_c = max(self.LR_C_STABLE,
-                        self.LR_C * np.power(self.LR_DECAY, ((self.pointer - 3000) / self.LR_DECAY_TIME)))
+        self.lr_a = max(self.LR_A_STABLE, self.LR_A * np.power(self.LR_DECAY, ((self.pointer - 3000) / self.LR_DECAY_TIME)))
+        self.lr_c = max(self.LR_C_STABLE, self.LR_C * np.power(self.LR_DECAY, ((self.pointer - 3000) / self.LR_DECAY_TIME)))
         self.show_lar_a = self.lr_a
 
         if (not tranLock) or self.pointer < self.var_end_at:
@@ -253,34 +255,35 @@ class TD3(object):
             with torch.no_grad():
                 # 计算扰动噪声后的动作a_ (没有用师兄原本的噪声， 用的td3 的噪声
                 if self.is_smooth:
+
                     # noise = (torch.randn_like(torch.FloatTensor(b_a)) * self.policy_noise).clamp(-self.a_bound,self.a_bound)
                     # action_with_noise = (self.actor_target(b_s_) + noise).clamp(-self.a_bound,self.a_bound)
 
                     sample = torch.distributions.Normal(0., 1.)
                     a_dim = (self.a_dim,)
                     sample_ = sample.sample(a_dim)
-                    noise = torch.clamp(sample_ * self.eval_noise_scale, -2 * self.eval_noise_scale,
-                                        2 * self.eval_noise_scale)
-                    action_with_noise = (self.actor_target(b_s_) + noise).clamp(-self.a_bound, self.a_bound)
+                    noise = torch.clamp(sample_ * self.eval_noise_scale,-2 * self.eval_noise_scale,2 * self.eval_noise_scale)
+                    action_with_noise = (self.actor_target(b_s_)+noise).clamp(-self.a_bound,self.a_bound)
                 # 计算target Q 值
-                target_Q1, target_Q2 = self.critic_target(b_s_, action_with_noise)
+                target_Q1,target_Q2 = self.critic_target(b_s_,action_with_noise)
                 target_Q = torch.min(target_Q1, target_Q2)
                 # target_Q = torch.tensor(b_r) + self.GAMMA * target_Q * self.discount
                 target_Q = torch.tensor(b_r) + self.GAMMA * target_Q
 
             # 获得当前batch Q estimates
-            current_Q1, current_Q2 = self.critic(b_s, b_a)
+            current_Q1,current_Q2 = self.critic(b_s,b_a)
             # 计算critic loss = td - error
-            self.critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+            self.critic_loss = F.mse_loss(current_Q1,target_Q) +F.mse_loss(current_Q2,target_Q)
 
             # Optimize the critic
             self.critic_optimizer.zero_grad()
             self.critic_loss.backward()
             self.critic_optimizer.step()
 
+
             # 延迟策略更新
             if self.update_cnt % self.policy_target_update_interval == 0:
-                self.actor_loss = -self.critic.Q1(b_s, self.actor(b_s)).mean()
+                self.actor_loss = -self.critic.Q1(b_s,self.actor(b_s)).mean()
                 self.actor_optimizer.zero_grad()
                 self.actor_loss.backward()
                 self.actor_optimizer.step()
@@ -330,19 +333,18 @@ class TD3(object):
                     action_with_noise = (self.actor_target(b_s_) + noise).clamp(-self.a_bound, self.a_bound)
 
                 # 计算target Q 值
-                target_Q1, target_Q2 = self.critic_target(b_s_, action_with_noise)
+                target_Q1,target_Q2 = self.critic_target(b_s_,action_with_noise)
                 target_Q = torch.min(target_Q1, target_Q2)
                 # target_Q = torch.tensor(b_r) + self.GAMMA * target_Q * self.discount
                 target_Q = torch.tensor(b_r) + self.GAMMA * target_Q
             # 获得当前batch Q estimates
-            current_Q1, current_Q2 = self.critic(b_s, b_a)
+            current_Q1,current_Q2 = self.critic(b_s,b_a)
             # 计算critic loss = td - error
             self.critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
         return self.critic_loss
 
         # 将经验存储到缓冲池（memory）中
         # 参数：时间步数（或者称作episode的索引）、当前状态、选择的动作、获得的奖励以及下一个状态
-
     def store_transition(self, h_epi, s, a, r, s_):
         # transition = np.hstack((s, a, [r], s_))
         # index = self.pointer % MEMORY_CAPACITY  # replace the old memory with new memory
@@ -350,12 +352,12 @@ class TD3(object):
         self.pointer += 1  # 存储了一条新经验
         self.memory.store_transition(h_epi, s, a, r, s_)  # 将当前状态、动作、奖励和下一个状态作为一条完整的经验（transition）存储到经验缓冲池中
 
-    def translate(self, data):
+    def translate(self,data):
         for d in range(len(data)):
-            data[d] = data[d] / 1000
+            data[d] = data[d]/1000
         return np.array(data)
 
-    def set_global_seed(self, seed):
+    def set_global_seed(self,seed):
         # pytorch_seed
         torch.manual_seed(seed)
         if torch.cuda.is_available():
@@ -364,7 +366,7 @@ class TD3(object):
         # python_seed
         random.seed(seed)
 
-        # NumPy_seed
+        #NumPy_seed
         np.random.seed(seed)
 
         # 设置python的hash随机种子
@@ -377,7 +379,9 @@ class TD3(object):
         return self.var
 
     def get_loss(self):
-        return self.critic_loss, self.actor_loss
+        return self.critic_loss,self.actor_loss
+
+
 
 
 class Memory(object):
@@ -385,20 +389,21 @@ class Memory(object):
         self.capacity = capacity
         self.data = np.zeros((capacity, dims))
         self.pointer = 0
-        self.exp_rep = ExpRep(capacity, 1, False, False, False)
-        self.sample_rate = 0.03
+        self.exp_rep = ExpRep(capacity, 1, False,False,False)
+        self.sample_rate=0.03
 
-    def store_transition(self, h_epi, s, a, r, s_):
-        # 在此处将array转为c_int64
+    def store_transition(self,h_epi, s, a, r, s_):
+        #在此处将array转为c_int64
         a = self.exp_rep.encoded_actions(action=a)
 
-        return self.exp_rep.record(h_epi, s, a, r, s_)
+        return self.exp_rep.record(h_epi,s,a,r,s_)
+
 
     def sample(self, n):
         # assert self.pointer >= self.capacity, 'Memory has not been fulfilled'
         # indices = np.random.choice(self.capacity, size=n)
         # return self.data[indices, :]
-        return self.exp_rep.get_random_batch(n, self.sample_rate)
+        return self.exp_rep.get_random_batch(n,self.sample_rate)
 
     def new_ep(self):
         return self.exp_rep.new_episode()
